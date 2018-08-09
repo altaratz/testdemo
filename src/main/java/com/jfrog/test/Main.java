f7536fbc9473fc7cf5e2f2212d880fb7f3b7f64d
altaratz <udi.altaratz@logmein.com>
2018-08-09 18:22:22 +0300

    Second

--- src/main/java/com/jfrog/test/Main.java
+++ src/main/java/com/jfrog/test/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Die die = new Die();
+        System.out.println("hello");
 
         System.out.println(die.getStr());
     }
