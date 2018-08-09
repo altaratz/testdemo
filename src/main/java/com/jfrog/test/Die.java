2821625e2cb71841fcf1e3a4b857a1b19de6b12a
altaratz <udi.altaratz@logmein.com>
2018-08-09 18:08:12 +0300

    This is my first commit ever :-)

---
+++ src/main/java/com/jfrog/test/Die.java
@@ -0,0 +1,17 @@
+package com.jfrog.test;
+
+/**
+ * Created by iland on 8/9/2018.
+ */
+public class Die {
+
+    private String str = "Udi = dude";
+
+    public Die() {
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+}
---
+++ src/main/java/com/jfrog/test/Main.java
@@ -0,0 +1,15 @@
+package com.jfrog.test;
+
+/**
+ * Created by iland on 8/9/2018.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        Die die = new Die();
+
+        System.out.println(die.getStr());
+    }
+
+}
