7d41d4bd9619ea58c98ec0bd2487953ebf16b131
altaratz <udi.altaratz@logmein.com>
2018-08-12 15:59:49 +0300

    for testing jenkins

---
+++ src/main/java/com/jfrog/test/DieBye.java
@@ -0,0 +1,17 @@
+package com.jfrog.test;
+
+/**
+ * Created by iland on 8/9/2018.
+ */
+public class DieBye {
+
+    private String str = "Udi = test1";
+
+    public DieBye() {
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+}
--- src/main/java/com/jfrog/test/Main.java
+++ src/main/java/com/jfrog/test/Main.java
@@ -8,10 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
         Die die = new Die();
+        DieBye diabye = new DieBye();
         System.out.println("hello");
         System.out.println("try jenkins");
 
         System.out.println(die.getStr());
+        System.out.println(diabye.getStr());
     }
 
 }
