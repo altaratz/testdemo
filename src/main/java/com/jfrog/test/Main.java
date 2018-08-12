7afcead163d8bf308336c69282cdfd927b00eea9
altaratz <udi.altaratz@logmein.com>
2018-08-12 15:05:14 +0300

    testing Jenkins build job

--- src/main/java/com/jfrog/test/Main.java
+++ src/main/java/com/jfrog/test/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Die die = new Die();
         System.out.println("hello");
+        System.out.println("try jenkins");
 
         System.out.println(die.getStr());
     }
